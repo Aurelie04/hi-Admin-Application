f7b9ff744cf3f6a67b9bf909182903c524fa086e
Aurelie04 <aurelienana25@gmail.com>
2025-01-29 17:41:55 +0200

    introducing the project

---
+++ src/main/java/com/aurelie/hiadmin/hi_admin/HiAdminApplication.java
@@ -0,0 +1,13 @@
+package com.aurelie.hiadmin.hi_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aurelie/hiadmin/hi_admin/HiAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aurelie.hiadmin.hi_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
